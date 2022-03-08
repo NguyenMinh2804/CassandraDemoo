1f063c44afe45f7f5960b1ebe79c14fa6bf99e18
NguyenMinh2804 <46109106+NguyenMinh2804@users.noreply.github.com>
2022-03-09 00:30:56 +0700

    Add project files.

---
+++ CassandraDemoo/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Cassandra;
+using Cassandra.Mapping;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CassandraAzure.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private ICluster _cluster;
+        private ISession _session;
+        public UserController()
+        {
+            _cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+            _session = _cluster.Connect("tutorialspoint");
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            IMapper mapper = new Mapper(_session);
+            var users = mapper.Fetch<User>("Select * from users;").ToList();
+            return users;
+        }
+    }
+}
---
+++ CassandraDemoo/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CassandraAzure
+{
+    public class User
+    {
+        public int id { get; set; }
+        public String username { get; set; }
+        public String email { get; set; }
+    }
+}
